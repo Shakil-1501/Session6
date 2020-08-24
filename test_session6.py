ed3ff2afb197a0bda51454d423aaaeff0d016042
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-08-24 17:29:54 +0530

    Add files via upload

--- test_session6.py
+++ test_session6.py
@@ -33,12 +33,68 @@ def test_docstring():
     assert q1 is not None
 
 
-def check_annotation():
+def test_annotation():
     q1=session6.pokerstar.__annotations__
     assert q1 == {'no_of_card': 'int',
     'no_of_player': 'int',
     'return': 'winner of the game',
     'sequence_cardsA': 'list',
     'sequence_cardsB': 'list'}
+
+
+def test_created_cards():
+    q=session6.poker(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],['spades', 'clubs', 'hearts', 'diamonds'])
+    q==['2spades',
+    '2clubs',
+    '2hearts',
+    '2diamonds',
+    '3spades',
+    '3clubs',
+    '3hearts',
+    '3diamonds',
+    '4spades',
+    '4clubs',
+    '4hearts',
+    '4diamonds',
+    '5spades',
+    '5clubs',
+    '5hearts',
+    '5diamonds',
+    '6spades',
+    '6clubs',
+    '6hearts',
+    '6diamonds',
+    '7spades',
+    '7clubs',
+    '7hearts',
+    '7diamonds',
+    '8spades',
+    '8clubs',
+    '8hearts',
+    '8diamonds',
+    '9spades',
+    '9clubs',
+    '9hearts',
+    '9diamonds',
+    '10spades',
+    '10clubs',
+    '10hearts',
+    '10diamonds',
+    'jackspades',
+    'jackclubs',
+    'jackhearts',
+    'jackdiamonds',
+    'queenspades',
+    'queenclubs',
+    'queenhearts',
+    'queendiamonds',
+    'kingspades',
+    'kingclubs',
+    'kinghearts',
+    'kingdiamonds',
+    'acespades',
+    'aceclubs',
+    'acehearts',
+    'acediamonds']
 
 
